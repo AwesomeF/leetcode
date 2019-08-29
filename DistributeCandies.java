7f93a7c5789d3752c50826ce929d16b6b02078b6
zhouwu <53322534+AwesomeF@users.noreply.github.com>
2019-08-29 09:32:40 +0800

    Update DistributeCandies.java

--- DistributeCandies.java
+++ DistributeCandies.java
@@ -3,15 +3,25 @@ public class DistributeCandies {
     public static void main(String[] args) {
 
         int candies = 1000;
-        int people = 20;
+        int people = 10;
 
         int[] result = giveCandies(candies, people);
-        for (int i : result) {
-            System.out.print(i + " ");
+        if (result != null) {
+            for (int i : result) {
+                System.out.print(i + " ");
+            }
+        } else {
+            System.out.println(" input candies or num_people invalid");
         }
+
     }
 
     private static int[] giveCandies(int candies, int people) {
+//        1 <= candies <= 10^9
+//        1 <= num_people <= 1000
+        if (people < 1 || people > 1000 || candies < 1) {
+            return null;
+        }
         int currentCount = 1;
         int peopleIndex = 0;
         int currentLeft = candies;
