575401983699d8de16e0513673784946f0b16cff
zhouwu.huang <zhouwu.huang@alo7.com>
2019-09-26 10:36:18 +0800

    添加删树成林算法

--- src/DeleteTreeNode.java
+++ src/DeleteTreeNode.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 给出二叉树的根节点 root，树上每个节点都有一个不同的值。
@@ -28,44 +30,57 @@ import java.util.List;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/delete-nodes-and-return-forest
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-
+ * <p>
  * Definition for a binary tree node.
  * public class TreeNodeOperate {
- *     int val;
- *     TreeNodeOperate left;
- *     TreeNodeOperate right;
- *     TreeNodeOperate(int x) { val = x; }
+ * int val;
+ * TreeNodeOperate left;
+ * TreeNodeOperate right;
+ * TreeNodeOperate(int x) { val = x; }
  * }
  */
 
 public class DeleteTreeNode {
 
 
-
-    private static List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        List<TreeNode> treeNodes = new ArrayList<>();
-
-        for (int delVal : to_delete) {
-            List<TreeNode> newTreeNodes = new ArrayList<>();
-            for (TreeNode node : treeNodes) {
-                TreeNode nodeRecord = node;
-                while (node.val != delVal) {
-                    if (node.left != null) {
-
-                    }
-                    if (node.left == null && node.right == null) {
-                        break;
-                    } else if (node.left != null) {
-                        node = node.left;
-                    }
-                }
-            }
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        List<TreeNode> forest = new ArrayList<>();
+        if (null == root) {
+            return forest;
         }
-        return treeNodes;
+        Set<Integer> toDeleteSet = new HashSet<>();
+        for (int val : to_delete) {
+            toDeleteSet.add(val);
+        }
+        root = recursionDelNodes(root, forest, toDeleteSet);
+        if (null != root) {
+            forest.add(root);
+        }
+        return forest;
+
+    }
+
+    //递归
+    public TreeNode recursionDelNodes(TreeNode root, List<TreeNode> forest, Set<Integer> toDeleteSet) {
+        if (null == root) {
+            return null;
+        }
+        root.left = recursionDelNodes(root.left, forest, toDeleteSet);
+        root.right = recursionDelNodes(root.right, forest, toDeleteSet);
+        if (toDeleteSet.contains(root.val)) {
+            if (null != root.left) {
+                forest.add(root.left);
+            }
+            if (null != root.right) {
+                forest.add(root.right);
+            }
+            return null;
+        }
+        return root;
     }
 
 
-    public class TreeNode{
+    public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
